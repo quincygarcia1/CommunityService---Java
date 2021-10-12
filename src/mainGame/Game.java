3e605a07fe493bc1efefbdf2506b7af194d21db3
quincygarcia1 <quincygarcia1@gmail.com>
2021-10-12 15:33:05 -0400

    added the JavaFX library custom to the project

--- src/mainGame/Game.java
+++ src/mainGame/Game.java
@@ -1,10 +1,28 @@
 package mainGame;
 
-public class Game {
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
+public class Game extends Application{
+
+	private Pane root = new Pane();
+	
+	private Parent createContent() {
+		root.setPrefSize(1400, 700);
+		return root;
+	}
+	public void start(Stage stage) throws Exception {
+		stage.setTitle("Community Service");
+		stage.setScene(new Scene(createContent()));
+		stage.show();
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		launch(args);
 	}
 
 }
