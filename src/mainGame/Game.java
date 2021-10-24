1a9dab1967641816e90b73f68b03ac208aceab33
quincygarcia1 <quincygarcia1@gmail.com>
2021-10-24 00:53:55 -0400

    some light work. Started main menu
    Just started working with some of the features to make a main menu and stuff. Nothing too big

--- src/mainGame/Game.java
+++ src/mainGame/Game.java
@@ -3,22 +3,34 @@ package mainGame;
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Game extends Application{
-
-	private Pane root = new Pane();
 	
-	private Parent createContent() {
-		root.setPrefSize(1400, 700);
-		return root;
-	}
+	Stage window;
+	Scene startScreen, settingsMenu, shop, game;
+	
+		
 	public void start(Stage stage) throws Exception {
-		stage.setTitle("Community Service");
-		stage.setScene(new Scene(createContent()));
-		stage.show();
+		window = stage;
+		window.setTitle("Community Service");
+		Pane root = new Pane();
+		int width = 700;
+		int height = 500;
+		root.setPrefSize(width, height);
+		Label title = new Label(String.valueOf(root.getLayoutX()));
+		Button startButton = new Button("Start");
+		startButton.setLayoutX(width/2 - 10);
+		startButton.setLayoutY(height/2);
+		root.getChildren().addAll(title, startButton);
+		startScreen = new Scene(root);
+		window.setScene(startScreen);
+		window.show();
 	}
+	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
